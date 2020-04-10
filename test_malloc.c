95733c370d717aebde99a5508b8430ac0528c8a0
Hinka Patel <hinkapatel2994@gmail.com>
2020-04-10 10:24:19 -0700

    added linkedlist optimization for malloc to get block from linkedlist

---
+++ test_malloc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "malloc_block_list.c"
+//#include <stdlib.h>
+#define NUM_ITERATIONS 100000
+int main() {
+    int index = 0;
+    void *p[NUM_ITERATIONS];
+
+    for(index = 0; index < NUM_ITERATIONS; index++) {
+        p[index] = malloc(5000); //allocate 5000 bytes.
+    }
+
+    for(index = 0; index < NUM_ITERATIONS; index++) {
+        free(p[index]);
+    }
+    return 0;
+}
